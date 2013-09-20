ac84b3fb7cafd5ab8ab7f356a7f2cc3a3b7ef58a
GongT <GongT@localhost.localdomain>
2013-09-20 18:09:21 +0800

    服务器取消调试

--- App/compile.php
+++ App/compile.php
@@ -4,17 +4,20 @@ define('APP_NAME', 'app');
 define('APP_PATH', __DIR__ . '/');
 define('LIB_PATH', APP_PATH);
 
-define('APP_DEBUG', true);
-define('CORE_DEBUG', true);
-define('SHOW_TRACE', true);
-define('TRACE_DEBUG', false);
-define('TMPL_NO_CACHE', true);
-define('TMPL_READABLE', true);
-define('JS_DEBUG', true);
-define('STATIC_DEBUG', true);
-define('MEMORY_DEBUG', true);
-define('LESS_DEBUG', true);
-
+if(is_file(dirname(__DIR__) . '/status.php')){
+	require dirname(__DIR__) . '/status.php';
+} else{
+	define('APP_DEBUG', true);
+	define('CORE_DEBUG', true);
+	define('SHOW_TRACE', true);
+	define('TRACE_DEBUG', false);
+	define('TMPL_NO_CACHE', true);
+	define('TMPL_READABLE', true);
+	define('JS_DEBUG', true);
+	define('STATIC_DEBUG', true);
+	define('MEMORY_DEBUG', true);
+	define('LESS_DEBUG', true);
+}
 
 require dirname(__DIR__) . '/defines.php';
 include 'mytp_include.php';
