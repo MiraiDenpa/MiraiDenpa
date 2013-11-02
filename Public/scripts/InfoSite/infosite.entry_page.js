42f582f95ad49467bd3ade8418ca34ec3cdbe7e7
GongT <gongteng524702837@gmail.com>
2013-11-02 23:32:52 +0800

    详情页结构最终完成

--- Public/scripts/InfoSite/infosite.entry_page.js
+++ Public/scripts/InfoSite/infosite.entry_page.js
@@ -33,18 +33,75 @@ $(function (){
 			return;
 		}
 		var container = $('.vote_large');
-		container.prepend($('<pre/>').text(JSON3.stringify(vote_catelog, null, 4)));
-		console.log(current_data);
 		$(vote_catelog).each(function (_, catelog){
 			if(catelog.when){
 				var ret = new MongoLike(catelog.when).test(current_data);
-				console.log(catelog.when, ret);
+				// console.log(catelog.when, ret);
 				if(!ret){
-					//delete(vote_catelog[_]);
+					delete(vote_catelog[_]);
+					return;
 				}
 			}
+
+			var bar;
+			switch(catelog.type){
+			case 0:
+				bar = new window.components.ValueBar('success');
+				// 中央文字 - 评分名字
+				covertext(catelog.name, 'center').addClass('revert').appendTo(bar);
+				// 好评差评
+				covertext(catelog.values[0], 'left').appendTo(bar);
+				covertext(catelog.values[1], 'right').appendTo(bar);
+				break;
+			case 1:
+				bar = new window.components.CenterBar('warning', 'success');
+				// 中央文字 - 评分名字
+				covertext(catelog.name, 'center').addClass('revert').appendTo(bar);
+				// 好评
+				covertext(catelog.values[0], 'left').appendTo(bar);
+				// 中评
+				covertext(catelog.values[1], 'center').appendTo(bar);
+				// 差评
+				covertext(catelog.values[2], 'right').appendTo(bar);
+				break;
+			case 2:
+				bar = new window.components.TwoSideBar('danger', 'success');
+				// 中央文字 - 评分名字
+				covertext(catelog.name, 'center').addClass('revert').appendTo(bar);
+				// 好评
+				covertext(catelog.values[0], 'left').appendTo(bar);
+				// 中评
+				covertext(catelog.values[1], 'center').appendTo(bar);
+				// 差评
+				covertext(catelog.values[2], 'right').appendTo(bar);
+				break;
+			default :
+				throw new Error('未知类型：' + catelog.type);
+			}
+
+			bar.addClass('hovershow trans-opacity');
+
+			bar.offset = catelog.offset;
+			bar.appendTo($('<div class="voteitem col-md-6"/>').appendTo(container));
+
+			window.bars.push(bar);
 		});
 	}
 
+	function covertext(text, textdir){
+		return $('<span class="full_cover"/>').addClass('text-' + textdir + ' hovershow-item').text(text);
+	}
+
+	var dg = new window.components.DragPointer(80);
+
+	window.test = function (){
+		for(var i in bars){
+			bars[i].left = parseInt(Math.random()*100);
+			bars[i].right = parseInt(Math.random()*100);
+			bars[i].value = parseInt(Math.random()*100);
+		}
+	};
+
+	window.bars = [];
 	setTimeout(instance_vote, 0);
 });
--- Public/scripts/InfoSite/infosite.entry_vote.js
+++ Public/scripts/InfoSite/infosite.entry_vote.js
@@ -1,24 +1,72 @@
 (function (components){
 	"use strict";
-	
-	
-	
-	components.ValueBar = function (cls){
 
+	components.ValueBar = function (cls){
+		var $ret = $('<div class="progress" style="">');
+		var $bar = $('<div class="progress-bar progress-bar-' + cls + '" style="width:0;"/>');
+		$ret.append($bar);
+
+		var value = 0;
+		Object.defineProperty($ret, 'value', {
+			get: function (){
+				return value;
+			},
+			set: function (set){
+				value = set;
+				$bar.css({width: set + '%'});
+			}
+		});
+
+		return $ret;
 	};
 
-	components.CenterBar = function (cls){
+	components.CenterBar = function (class1, class2){
+		var $ret = new components.TwoSideBar(class1, class2);
 
+		var value = 0;
+		Object.defineProperty($ret, 'value', {
+			get: function (){
+				return value;
+			},
+			set: function (set){
+				if(set >= 50){
+					$ret.left = 0;
+					$ret.right = set;
+				} else{
+					$ret.left = -set;
+					$ret.right = 0;
+				}
+				value = set;
+			}
+		});
+
+		var offset = 0;
+		Object.defineProperty($ret, 'offset', {
+			get: function (){
+				return offset/10;
+			},
+			set: function (set){
+				offset = set*10;
+			}
+		});
+
+		return $ret;
 	};
 
 	components.TwoSideBar = function (class1, class2){
-		var $ret = $('<div class="progress" style="padding-left: 50%">');
-		var $left = $('<div class="progress-bar progress-bar-' + class1 + '" style="width:0;margin-left:0;"/>');
+		var $ret = $('<div class="progress" style="padding-left: 50%;">');
+		var $left = $('<div class="progress-bar progress-bar-' + class1 + ' revert" style="width:0;margin-left:0;"/>');
 		var $right = $('<div class="progress-bar progress-bar-' + class2 + '" style="width:0"/>');
-		var $lLabel = $('<span/>').appendTo($left);
-		var $rLabel = $('<span/>').appendTo($right);
 
 		$ret.append($left).append($right);
+
+		$ret.mapPosition = function ($obj, value){
+			if(value > 50){
+				$obj.css({width: (value - 50)*2 + '%'});
+			} else{
+				$obj.css({width: value + '%', marginLeft: '-' + value + '%'});
+			}
+		};
 
 		var left = 0;
 		Object.defineProperty($ret, 'left', {
@@ -44,4 +92,88 @@
 
 		return $ret;
 	};
+
+	components.DragPointer = function DragPointer(size){
+		var container = $('<div class="drag_pointer">');
+		var content = $('<div class="circle"/>').appendTo(container);
+		var arrow = $('<div class="arrow"/>').appendTo(container);
+
+		Object.defineProperty(this, 'content', {
+			get: function (){
+				return content;
+			}
+		});
+
+		// 计算 vvv
+		var x = 0;
+		Object.defineProperty(this, 'x', {
+			set: function (ox){
+				x = ox;
+				recalc();
+			},
+			get: function (){
+				return x;
+			}
+		});
+		var y = 0;
+		Object.defineProperty(this, 'y', {
+			set: function (oy){
+				y = oy;
+				recalc();
+			},
+			get: function (){
+				return y;
+			}
+		});
+		var d = size/2;
+		Object.defineProperty(this, 'size', {
+			set: function (osize){
+				size = osize;
+				d = size/2;
+				recalc();
+			},
+			get: function (){
+				return size;
+			}
+		});
+		var rotate = 0, alpha = (135 - rotate)*Math.PI/180;
+		Object.defineProperty(this, 'rotate', {
+			set: function (orotate){
+				rotate = orotate;
+				alpha = (135 - orotate)*Math.PI/180;
+				recalc();
+			},
+			get: function (){
+				return rotate;
+			}
+		});
+
+		var sqrt2 = Math.sqrt(2);
+
+		function recalc(){
+			var xx = x - sqrt2*d*Math.cos(alpha) - d;
+			var yy = y + sqrt2*d*Math.sin(alpha) - d;
+
+			container.css({height: size, width: size, left: xx, top: yy});
+			arrow.css('transform', 'rotate(' + rotate + 'deg)');
+		}
+
+		var s = this;
+		$(['appendTo', 'prependTo', 'insertAfter', 'insertBefore', 'remove',
+			'data', 'removeData', 'css',
+			'anime', 'queue', 'dequeue', 'trigger', 'delay', 'on', 'off',
+			'fadeIn', 'fadeTo', 'fadeOut', 'fadeToggle', 'show', 'hide', 'toggle',
+			'class', 'addClass', 'hasClass', 'removeClass', 'toggleClass',
+			'parent', 'closest', 'offset'
+		])
+				.each(function (_, name){
+					s[name] = function (){
+						return $.fn[name].apply(container, arguments);
+					}
+				});
+		s = null;
+
+		recalc();
+		return this;
+	}
 })(window.components || (window.components = {}));
