4fb11ff88eccecfdce37772bcae30f1dfd569d8b
GongT <gongteng524702837@gmail.com>
2013-11-26 18:28:44 +0800

    添加上线配置

---
+++ User/Conf/release/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'MAIL_USER'    => '030@dianbo.me',
+	'MAIL_PASSWORD'    => '3DiV66ggSND9',
+	'MAIL_FROM'        => '030@dianbo.me',
+	'MAIL_FROM_NAME'    => '电波娘',
+	'MAIL_HOST'    => 'smtp.exmail.qq.com:465',
+	'MAIL_HOST_SECURE'    => 'ssl',
+);
